6f71ad1c27d74c04bfe5556834641d034cd12cff
Bikram <bikramjit7777@gmail.com>
2024-06-27 21:22:06 -0400

    highlights of skillls section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,3 +1,87 @@
-export default function Skills() { 
-    return (<h1>Skills</h1>)
+export default function Skills() {
+  return (
+    <div className="box-shadow-full mb-0">
+      <h2 style={{ textAlign: "center" }}>SUMMARY OF SKILLS</h2>
+      <br />
+      <ul>
+        <li>
+          <strong>Proficiency with MERN Stack:</strong> Demonstrated experience
+          with MongoDB, Express, React, and Node, leveraging these technologies
+          to build robust and scalable web applications.
+        </li>
+        <li>
+          <strong>Frontend and Backend Expertise:</strong> Well-developed
+          knowledge of frontend and backend technologies including React,
+          NodeJS, and JavaScript (ES6), ensuring efficient development and
+          implementation of features.
+        </li>
+        <li>
+          <strong>Full Stack Development Skills:</strong> Proficient in HTML5,
+          CSS3, JavaScript, jQuery, ReactJS, NodeJS, and PHP, adept at UI
+          development and creating dynamic web applications.
+        </li>
+        <li>
+          <strong>NPM and Bootstrap:</strong> Understanding and utilization of
+          NPM libraries and Bootstrap for efficient development and styling of
+          web applications.
+        </li>
+        <li>
+          <strong>RESTful API Development:</strong> Experience in designing and
+          implementing RESTful APIs using NodeJS at the backend, enabling
+          seamless communication between frontend and backend systems.
+        </li>
+        <li>
+          <strong>Responsive and Accessible Web Design:</strong> Proficient in
+          designing and developing responsive and user-friendly websites with a
+          focus on web accessibility standards, ensuring inclusivity for all
+          users.
+        </li>
+        <li>
+          <strong>Debugging and Testing:</strong> Skilled in debugging using
+          browser developer tools in Chrome and Firefox, and testing APIs with
+          tools like Postman, ensuring the reliability and functionality of web
+          applications.
+        </li>
+        <li>
+          <strong>Database Management:</strong> Proficient in MySQL and MongoDB,
+          with hands-on experience in database management and optimization for
+          efficient data storage and retrieval.
+        </li>
+        <li>
+          <strong>Version Control and Hosting:</strong> Experienced in version
+          control using Git and GitHub, and website hosting on platforms like
+          Heroku, Firebase, and GoDaddy, ensuring efficient collaboration and
+          deployment.
+        </li>
+        <li>
+          <strong>Unit Testing and Project Management:</strong> Proficient in
+          unit testing with Jest, and experience with project management and
+          issue tracking tools like Jira and Trello, ensuring quality and timely
+          delivery of projects.
+        </li>
+        <li>
+          <strong>Agile Methodology:</strong> Knowledgeable in Agile Software
+          Development Life Cycle (SDLC), with experience working in Agile teams,
+          ensuring iterative and collaborative development processes.
+        </li>
+        <li>
+          <strong>Fundamentals of Programming:</strong> Solid understanding and
+          practical implementation of programming concepts including
+          object-oriented programming (OOP), loops, conditional statements,
+          functions, and exception handling.
+        </li>
+        <li>
+          <strong>Additional Language Proficiency:</strong> Experience working
+          with other languages like Java, Angular.js, Android, and C++,
+          expanding the scope of development capabilities.
+        </li>
+        <li>
+          <strong>Communication and Teamwork:</strong> Strong written and spoken
+          communication skills, excellent team player, self-motivated, and eager
+          to learn modern technologies, contributing to a positive and
+          collaborative work environment.
+        </li>
+      </ul>
+    </div>
+  );
 }
